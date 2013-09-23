5910f67d2f1a16e3ef71db4a2d17be23e6eefcd0
Kristjan Rang <mail@rang.ee>
2013-09-23 11:59:53 +0300

    First

---
+++ steam.go
@@ -0,0 +1,194 @@
+package steam
+
+import (
+	// "sort"
+	"strconv"
+	// "strings"
+	"encoding/xml"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var (
+	user string
+
+	steamApiRoot = "http://steamcommunity.com/id/"
+)
+
+type User struct {
+	SteamID64      string `xml:"steamID64"`
+	SteamID        string `xml:"steamID"`
+	OnlineState    string `xml:"onlineState"`
+	StateMessage   string `xml:"stateMessage"`
+	AvatarIcon     string `xml:"avatarIcon"`
+	AvatarMedium   string `xml:"avatarMedium"`
+	AvatarFull     string `xml:"avatarFull"`
+	CustomURL      string `xml:"customURL"`
+	MemberSince    string `xml:"memberSince"`
+	SteamRating    string `xml:"steamRating"`
+	HoursPlayed2Wk string `xml:"hoursPlayed2Wk"`
+	Location       string `xml:"location"`
+	Realname       string `xml:"realname"`
+	Summary        string `xml:"summary"`
+	GameCount      int
+	// MostPlayed     []MostPlayedGame `xml:"mostPlayedGames>mostPlayedGame"`
+}
+
+func (u User) FullURL() string {
+	return steamApiRoot + u.CustomURL
+}
+
+func (u User) RatingDescription() string {
+	title := "Playing on PS3"
+
+	switch u.SteamRating {
+	case "10":
+		title = "EAGLES SCREAM"
+	case "9":
+		title = "Still not 10"
+	case "8":
+		title = "COBRA KAI!"
+	case "7":
+		title = "Wax on, Wax off"
+	case "6":
+		title = "Oooh! Shiny!"
+	case "5":
+		title = "Halfway Cool"
+	case "4":
+		title = "Master of Nothing"
+	case "3":
+		title = "Shooting Blanks"
+	case "2":
+		title = "Nearly Lifeless"
+	case "1":
+		title = "El Terrible!"
+	}
+
+	return title
+}
+
+// type MostPlayedGame struct {
+//  Name          string `xml:"gameName"`
+//  Link          string `xml:"gameLink"`
+//  Icon          string `xml:"gameIcon"`
+//  Logo          string `xml:"gameLogo"`
+//  LogoSmall     string `xml:"gameLogoSmall"`
+//  HoursPlayed   string `xml:"hoursPlayed"`
+//  HoursOnRecord string `xml:"hoursOnRecord"`
+// }
+
+type GamesList struct {
+	Games []Game `xml:"games>game"`
+}
+
+type Game struct {
+	AppID           string `xml:"appID"`
+	Name            string `xml:"name"`
+	Logo            string `xml:"logo"`
+	StoreLink       string `xml:"storeLink"`
+	HoursLast2Weeks string `xml:"hoursLast2Weeks"`
+	HoursOnRecord   string `xml:"hoursOnRecord"`
+}
+
+type GamesByLast2Weeks GamesList
+type GamesByHours GamesList
+
+// yes, this is bad
+func (s GamesByLast2Weeks) Less(i, j int) bool {
+	a, _ := strconv.ParseFloat(s.Games[i].HoursLast2Weeks, 64)
+	b, _ := strconv.ParseFloat(s.Games[j].HoursLast2Weeks, 64)
+
+	return a > b
+}
+
+func (s GamesByHours) Less(i, j int) bool {
+	a, _ := strconv.ParseFloat(s.Games[i].HoursOnRecord, 64)
+	b, _ := strconv.ParseFloat(s.Games[j].HoursOnRecord, 64)
+
+	return a > b
+}
+
+func (s GamesByLast2Weeks) Len() int      { return len(s.Games) }
+func (s GamesByLast2Weeks) Swap(i, j int) { s.Games[i], s.Games[j] = s.Games[j], s.Games[i] }
+
+func (s GamesByHours) Len() int      { return len(s.Games) }
+func (s GamesByHours) Swap(i, j int) { s.Games[i], s.Games[j] = s.Games[j], s.Games[i] }
+
+// func Update(user string) {
+// 	steamUserUpdate(user)
+// 	steamGamesUpdate(user)
+
+// 	UserData.Summary = strings.Replace(UserData.Summary, "<br>", "", -1)
+// 	UserData.GameCount = len(GamesData.Games)
+
+// 	playedLast2Weeks := []Game{}
+// 	notPlayedLastWeeks := []Game{}
+
+// 	for i := range GamesData.Games {
+// 		game := GamesData.Games[i]
+
+// 		if game.HoursLast2Weeks != "" {
+// 			playedLast2Weeks = append(playedLast2Weeks, game)
+// 		} else {
+// 			notPlayedLastWeeks = append(notPlayedLastWeeks, game)
+// 		}
+// 	}
+
+// 	sort.Sort(GamesByLast2Weeks{Games: playedLast2Weeks})
+// 	sort.Sort(GamesByHours{Games: notPlayedLastWeeks})
+
+// 	sortedGames := append(playedLast2Weeks, notPlayedLastWeeks...)
+
+// 	GamesData.Games = sortedGames[:5]
+
+// 	fmt.Println(" updated", time.Now())
+// }
+
+func SetConfig(u string) {
+	user = u
+}
+
+func GetUser() *User {
+	uri := steamApiRoot + user + "?xml=1"
+	userdata := &User{}
+	getData(uri, userdata)
+	return userdata
+}
+
+func GetGames(limit int) *[]Game {
+	uri := steamApiRoot + user + "/games/?xml=1"
+	gamedata := &GamesList{}
+	getData(uri, gamedata)
+	games := gamedata.Games[:limit]
+	return &games
+}
+
+// PRIVATE
+
+func getData(uri string, i interface{}) {
+	data := getRequest(uri)
+	xmlUnmarshal(data, i)
+}
+
+func getRequest(uri string) []byte {
+	res, err := http.Get(uri)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return body
+}
+
+func xmlUnmarshal(b []byte, i interface{}) {
+	err := xml.Unmarshal(b, i)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
